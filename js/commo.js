31a3116d5b6d32010d2640547fe3fed0768d11fa
tmoki0606 <6tomok6@gmail.com>
2026-07-30 23:12:06 +0900

    css,jsフォルダの追加

---
+++ js/commo.js
@@ -0,0 +1,21 @@
+(function(){
+    const nav = document.querySelector('.site-nav');
+    if (!nav) return;
+
+    let lastScrollY = window.scrollY;
+    const upThreshold = 10; //これ以上上にスクロールすると再表示
+
+    window.addEventListener('scroll', function(){
+        const currentScrollY = window.scrollY;
+
+        if (currentScrollY > lastScrollY && currentScrollY > nav.offsetHeight){
+            // 下にスクロール→隠す
+            nav.classList.add('nav-hidden');
+        } else if (lastScrollY - currentScrollY > upThreshold){
+            // 上に一定量スクロール→再表示
+            nav.classList.remove('nav-hidden');
+        }
+    
+    lastScrollY = currentScrollY;
+    });
+});
